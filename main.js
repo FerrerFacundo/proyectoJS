d8f00fe956bd94000777ea07350af52118c23454
Facundo <ferrerfacundo1@gmail.com>
2022-10-31 12:45:55 -0300

    Agrego cuotas y modifico textos

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 let producto = parseInt(
     prompt(
-        'Bienvenido a porcelaneste, elegi la manualidad de tu gusto ingresando el numero de la categoria: 1 Centro de tortas $4500 // 2 Souvenires $550 // 3 Accesorios $100'
+        'Bienvenido a Porcelaneste, elegi la manualidad de tu gusto ingresando el numero de la categoria: 1 Centro de tortas $4500 // 2 Souvenires $550 // 3 Accesorios $100'
     )
 )
 let totalCompra = 0
@@ -23,7 +23,7 @@ while (seguirComprando === true) {
     }
 
     //Pregunta si continua comprando, si contesta 1, vuelve a ofrecer productos, sino continua a la seccion cuotas
-    decision = parseInt(prompt('¿Continuar comprando? 1- Si  2- No'))
+    decision = parseInt(prompt('¿Continuar comprando? 1 Si // 2 No'))
     if (decision === 1) {
         producto = parseInt(
             prompt(
@@ -35,7 +35,7 @@ while (seguirComprando === true) {
     }
 }
 
-//Pregunto si quiere pagar en cuotas o contado
+//Pregunto si quiere pagar en cuotas o contado, sin ingresa 1, hace la funcion para calcular cuotas, si ingresa 2, informa el Total de la compra
 let decisionCuotas
 
 decisionCuotas = parseInt(prompt('¿Desea abonar en cuotas? 1 Si // 2 No'))
@@ -60,8 +60,6 @@ if (decisionCuotas === 1) {
         let resultado = dato1 / dato2;
         return resultado
     }
-
-    //Capturar el resultados de las cuotas
     let division = dividir(totalCompra, cuotas)
 
     //Mostrar resultado en console y al usuario
@@ -71,32 +69,3 @@ if (decisionCuotas === 1) {
 } else {
     alert("Usted debe pagar $" + totalCompra + " al contado")
 }
-
-
-    /*
-    //Funcion para ingresar cuotas a pagar
-    function cuotas1() {
-        let cuotas1 = parseInt(prompt("¿En cuantas cuotas quiere pagar, elija entre 1 a 12 cuotas por mes"));
-        alert("¿Desea pagar en " + cuotas1 + " cuotas?")
-        return cuotas1
-    }
-
-    //Capturar resultado de la cantidad de cuotas seleccionadas
-    let cuotas = cuotas1()
-
-    //Mostrar resultado de funciones en console para control
-    console.log("El total es: " + totalCompra)
-    console.log("Las cuotas son:" + cuotas)
-
-    //Dividir cantidad en cuotas
-    function dividir(dato1, dato2) {
-        let resultado = dato1 / dato2;
-        return resultado
-    }
-
-    //Capturar el resultados de las cuotas
-    let division = dividir(totalCompra, cuotas)
-
-    //Mostrar resultado en console y al usuario
-    console.log("Usted debe pagar " + cuotas + " cuotas de " + division)
-    alert("Usted debe pagar " + cuotas + " cuotas de $" + division)*/
